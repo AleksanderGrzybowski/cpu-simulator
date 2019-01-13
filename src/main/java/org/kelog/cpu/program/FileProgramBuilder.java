e9516320cc1e348c2898adac23048492d2c3ab2e
Aleksander Grzybowski <ifyouseethis@tellmeplease.it>
2019-01-13 19:46:02 +0100

    Refactor program builder a bit

--- src/main/java/org/kelog/cpu/program/FileProgramBuilder.java
+++ src/main/java/org/kelog/cpu/program/FileProgramBuilder.java
@@ -8,6 +8,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import static java.lang.Integer.parseInt;
 import static java.util.stream.Collectors.toList;
@@ -18,49 +19,81 @@ public class FileProgramBuilder {
         return new Program(parse(Files.readAllLines(Paths.get(filename))));
     }
     
-    public Program loadFromString(String content) throws IOException {
+    public Program loadFromString(String content) {
         return new Program(parse(Arrays.stream(content.split("\n")).collect(toList())));
     }
     
-    private List<ProgramInstruction> parse(List<String> inputLines) throws IOException {
-        List<String> lines = inputLines.stream()
-                .filter(line -> line.trim().length() != 0)
-                .map(String::trim)
-                .collect(toList());
-        
+    private List<ProgramInstruction> parse(List<String> lines) {
         return lines.stream()
-                .map(line -> toProgramInstruction(line))
+                .map(String::trim)
+                .filter(line -> !line.isEmpty())
+                .map(this::toProgramInstruction)
                 .collect(toList());
     }
     
     private ProgramInstruction toProgramInstruction(String line) {
-        String label = null;
-        if (line.contains(":")) {
-            label = line.substring(0, line.indexOf(":"));
-            line = line.substring(line.indexOf(":") + 1);
-            line = line.trim();
-        }
-    
-        String mnemonic, parameters;
-        mnemonic = parameters = null;
-        parameters = "";
+        String label, instruction;
         
-        if (!line.contains(" ")) { // no-arg operation
-            mnemonic = line;
+        if (line.contains(":")) {
+            int labelDelimiterIndex = line.indexOf(":");
+            
+            label = line.substring(0, labelDelimiterIndex);
+            instruction = line.substring(labelDelimiterIndex + 1).trim();
         } else {
-            mnemonic = line.substring(0, line.indexOf(" "));
-            parameters = line.substring(line.indexOf(" ") + 1).trim();
+            label = null;
+            instruction = line;
+        }
+        
+        String mnemonic, parameters;
+        
+        if (!instruction.contains(" ")) { // no-arg operation
+            mnemonic = instruction;
+            parameters = "";
+        } else {
+            int mnemonicDelimiterIndex = instruction.indexOf(" ");
+            
+            mnemonic = instruction.substring(0, mnemonicDelimiterIndex);
+            parameters = instruction.substring(mnemonicDelimiterIndex + 1).trim();
         }
         
         String firstArg, secondArg;
-        firstArg = secondArg = null;
         
         if (parameters.contains(",")) {
-            firstArg = parameters.substring(0, parameters.indexOf(",")).trim();
-            secondArg = parameters.substring(parameters.indexOf(",") + 1).trim();
+            int argumentDelimiterIndex = parameters.indexOf(",");
+            
+            firstArg = parameters.substring(0, argumentDelimiterIndex).trim();
+            secondArg = parameters.substring(argumentDelimiterIndex + 1).trim();
         } else {
             firstArg = parameters;
+            secondArg = null;
         }
+        
+        // 0-argument operations
+        
+        if (mnemonic.equals("exit")) {
+            return new ProgramInstruction(label, new Exit());
+        }
+        
+        // 1-argument operations
+        
+        if (mnemonic.equals("jmp")) {
+            return new ProgramInstruction(label, new Jump(firstArg));
+        }
+        
+        if (mnemonic.equals("je")) {
+            return new ProgramInstruction(label, new JumpIfEqual(firstArg));
+        }
+        
+        if (mnemonic.equals("jg")) {
+            return new ProgramInstruction(label, new JumpIfGreater(firstArg));
+        }
+        
+        if (mnemonic.equals("jl")) {
+            return new ProgramInstruction(label, new JumpIfLess(firstArg));
+        }
+        
+        // 2-argument operations
+        Objects.requireNonNull(secondArg);
         
         if (mnemonic.equals("add")) {
             if (secondArg.startsWith("r")) {
@@ -94,25 +127,6 @@ public class FileProgramBuilder {
             }
         }
         
-        if (mnemonic.equals("jmp")) {
-            return new ProgramInstruction(label, new Jump(firstArg));
-        }
-        
-        if (mnemonic.equals("je")) {
-            return new ProgramInstruction(label, new JumpIfEqual(firstArg));
-        }
-        
-        if (mnemonic.equals("jg")) {
-            return new ProgramInstruction(label, new JumpIfGreater(firstArg));
-        }
-        
-        if (mnemonic.equals("jl")) {
-            return new ProgramInstruction(label, new JumpIfLess(firstArg));
-        }
-        
-        if (mnemonic.equals("exit")) {
-            return new ProgramInstruction(label, new Exit());
-        }
         
         throw new AssertionError("Unknown instruction " + mnemonic);
     }
